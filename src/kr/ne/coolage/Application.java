92f49293d1f25ddaf1c4a4fa60cd3ad35b546821
ShaunPark <coolage73@gmail.com>
2019-11-01 14:56:22 +0900

    Initial commit

---
+++ src/kr/ne/coolage/Application.java
@@ -0,0 +1,11 @@
+package kr.ne.coolage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+}
